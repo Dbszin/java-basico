0fc28c8794c2f512cb29bd6a45f00cb35f268948
Dbszin <dodosilvabatista@gmail.com>
2024-09-18 10:12:42 -0300

    MÉTODOS

---
+++ METODOS/src/Usuarios.java
@@ -0,0 +1,11 @@
+
+public class Usuarios {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv Ligada?" + smartTv.ligada);
+        System.out.println("Canal Atual?" +smartTv.canal);
+        System.out.println("Tv Ligada?" +smartTv.volume);
+    }
+}
