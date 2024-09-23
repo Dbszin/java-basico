b7c07882c0745061216e8b385e65475d09bb4f89
Dbszin <dodosilvabatista@gmail.com>
2024-09-23 10:26:26 -0300

    MÉTODOS

--- METODOS/src/Usuarios.java
+++ METODOS/src/Usuarios.java
@@ -4,8 +4,49 @@ public class Usuarios {
 
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("Tv Ligada?" + smartTv.ligada);
-        System.out.println("Canal Atual?" +smartTv.canal);
-        System.out.println("Tv Ligada?" +smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual  : " + smartTv.volume);
+
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+
+
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual? " +smartTv.canal);
+        System.out.println("Volume Atual " +smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status - > TV Ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status - > Tv Ligada? " + smartTv.ligada);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
